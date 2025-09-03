cabd106bf2d3507bb16790a139d14ef9a1356fe9
lukeddy <luketzq@gmail.com>
2025-09-03 14:56:35 +0800

    Fix:修改配置解决Redis有密码报错NOAUTh Hello..异常

--- springboot-qrcode-login/src/main/java/com/example/qrcodelogin/config/RedisConfig.java
+++ springboot-qrcode-login/src/main/java/com/example/qrcodelogin/config/RedisConfig.java
@@ -2,34 +2,15 @@ package com.example.qrcodelogin.config;
 
 import com.fasterxml.jackson.annotation.JsonTypeInfo;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
-import org.springframework.data.redis.connection.RedisPassword;
-import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
-import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
 public class RedisConfig {
-
-    @Bean
-    public RedisConnectionFactory redisConnectionFactory(
-            @Value("${spring.redis.host}") String host,
-            @Value("${spring.redis.port}") int port,
-            @Value("${spring.redis.password}") String password,
-            @Value("${spring.redis.database}") int database) {
-        RedisStandaloneConfiguration redisConfig = new RedisStandaloneConfiguration();
-        redisConfig.setHostName(host);
-        redisConfig.setPort(port);
-        redisConfig.setPassword(RedisPassword.of(password));
-        redisConfig.setDatabase(database);
-
-        return new LettuceConnectionFactory(redisConfig);
-    }
 
     @Bean
     public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory connectionFactory) {
